43d1e9b0e2ee4a7ad9acf26c3e7299094a98043d
Abdul Ahmad <ahad.ahmad@mail.utoronto.ca>
2017-09-28 18:24:42 -0400

    Initial Code for PublicEnemy Added minor support for movement, shooting, collision.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int health = 50;
+    public int damageRate = 20;
+    public void DisableEnemy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Damage()
+    {
+        health -= damageRate;
+        if (health <= 0) DisableEnemy();
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Camera povCam;
+    public LineRenderer shootLine;
+    private WaitForSeconds gunTimeOut = new WaitForSeconds(0.5f);
+
+    // Use this for initialization
+    void Start()
+    {
+        shootLine = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentPosition = playerTransform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerTransform.position = new Vector3(currentPosition.x, currentPosition.y + 0.1f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerTransform.position = new Vector3(currentPosition.x - 0.1f, currentPosition.y);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerTransform.position = new Vector3(currentPosition.x, currentPosition.y - 0.1f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerTransform.position = new Vector3(currentPosition.x + 0.1f, currentPosition.y);
+        }
+
+        //TODO: Configure with Mouse position
+        if (Input.GetKey(KeyCode.Space))
+        {
+            RaycastHit gunShot;
+            Vector3 focus = povCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            StartCoroutine(Bullet());
+            shootLine.SetPosition(0, playerTransform.position);
+            if(Physics.Raycast(focus, new Vector3(0, 5), out gunShot, 100))
+            {
+                Debug.Log("Hit");
+                shootLine.SetPosition(1, gunShot.point);
+                var enemy = gunShot.collider.GetComponent<Health>();
+                if (enemy != null) enemy.DisableEnemy();
+            }
+            else
+            {
+                shootLine.SetPosition(1, focus + (new Vector3 (0, 5)* 100));
+            }
+        }
+    }
+
+    public IEnumerator Bullet()
+    {
+        shootLine.enabled = true;
+        yield return gunTimeOut;
+        shootLine.enabled = false;
+    }
+}
