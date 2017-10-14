027f274c82736700c7eaf2c5f05f1736d6676c80
Darren Hobin <darrenhobin@live.com>
2017-10-13 23:56:23 -0400

    Replace temporary laser with bullets
    This commit also contains minor non-functional changes.
    
    [#8]

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
-using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
@@ -9,16 +9,16 @@ public class Movement : MonoBehaviour
 	public static GameObject[] waypointArray;
 	public static int enemiesCount = 0;
 
-	public Camera povCam;
+	public float spawnInterval;
+	public float speed;
+	public GameObject bullet;
 	public LineRenderer shootLine;
-	public Transform playerTransform;
 
-	private WaitForSeconds gunTimeOut = new WaitForSeconds (0.5f);
+	private float lastBulletTime = 0f;
 
 	// Use this for initialization
 	void Start ()
 	{
-		shootLine = GetComponent<LineRenderer> ();
 		waypointArray = GameObject.FindGameObjectsWithTag ("Enemy");
 		Movement.enemiesCount = waypointArray.Length;
 	}
@@ -27,56 +27,41 @@ public class Movement : MonoBehaviour
 	void Update ()
 	{
 		//Update position of player
-		var currentPosition = playerTransform.position;
-		currentPosition.x += Input.GetAxisRaw ("Horizontal") * 5 * Time.deltaTime;
-		currentPosition.y += Input.GetAxisRaw ("Vertical") * 5 * Time.deltaTime;
-		playerTransform.position = new Vector3 (currentPosition.x, currentPosition.y);
+		var currentPosition = transform.position;
+		currentPosition.x += Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime;
+		currentPosition.y += Input.GetAxisRaw ("Vertical") * speed * Time.deltaTime;
+		transform.position = new Vector3 (currentPosition.x, currentPosition.y);
 
 		// Update player direction.
 		faceMouse ();
 
 		// Fire button is pressed.
 		if (Input.GetButton ("Fire1")) {
-			// For now, we are only shooting a gun, later we may be utilizing other types
-			// of weapons. So this may get replaced with a call to a function that determines
-			// which action to take.
+			// TODO: Different functions can be called based on weapon type. For now, just gun.
 			fireGun ();
 		}
-	}
-
-	public IEnumerator Bullet ()
-	{
-		shootLine.enabled = true;
-		yield return gunTimeOut;
-		shootLine.enabled = false;
 	}
 
 	private void faceMouse ()
 	{
 		// Get the in-world mouse position using the screen mouse position.
-		Vector3 mousePosition = povCam.ScreenToWorldPoint (Input.mousePosition);
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		// The direction vector between the character and the mouse.
-		playerTransform.up = new Vector3 (
-			mousePosition.x - playerTransform.position.x,
-			mousePosition.y - playerTransform.position.y,
+		transform.up = new Vector3 (
+			mousePosition.x - transform.position.x,
+			mousePosition.y - transform.position.y,
 			0
 		);
 	}
 
 	private void fireGun ()
 	{
-		Vector3 screenPos = povCam.ScreenToWorldPoint (Input.mousePosition);
-		RaycastHit2D hit = Physics2D.Raycast (screenPos, playerTransform.up, 100);
-		Vector3 focus = povCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0));
-		StartCoroutine (Bullet ());
-		shootLine.SetPosition (0, playerTransform.position);
-		if (hit) {
-			shootLine.SetPosition (1, hit.point);
-			var enemy = hit.collider.GetComponent<Health> ();
-			if (enemy != null)
-				enemy.Damage ();
-		} else {
-			shootLine.SetPosition (1, focus + (playerTransform.up * 100));
+		if (Time.time - lastBulletTime >= spawnInterval) {
+			lastBulletTime = Time.time;
+
+			var bulletIns = Instantiate (bullet, transform.position, transform.rotation);
+			bulletIns.transform.Translate (new Vector3 (0.303f, 0.738f, 0)); 
+			bulletIns.transform.Rotate (Vector3.forward * 90);
 		}
 	}
 
