2e1bd2ae4ccc8c3825f0cb609d90ae2dce59a9c5
Darren Hobin <darrenhobin@live.com>
2017-10-13 21:18:32 -0400

    Perform general clean up of Movement.cs
    This commit is in preparation of future commits for the same issue.
    
    [#8]

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,79 +5,79 @@ using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
-    public Transform playerTransform;
-    public Camera povCam;
-    public LineRenderer shootLine;
-    private WaitForSeconds gunTimeOut = new WaitForSeconds(0.5f);
-    public static GameObject[] waypointArray;
-    public static int enemiesCount = 0;
-    // Use this for initialization
-    void Start()
-    {
-        shootLine = GetComponent<LineRenderer>();
-        waypointArray = GameObject.FindGameObjectsWithTag("Enemy");
-        Movement.enemiesCount = waypointArray.Length;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Update position of player
-        var currentPosition = playerTransform.position;
-        currentPosition.x += Input.GetAxisRaw("Horizontal") * 5 * Time.deltaTime;
-        currentPosition.y += Input.GetAxisRaw("Vertical") * 5 * Time.deltaTime;
-        playerTransform.position = new Vector3(currentPosition.x, currentPosition.y);
+	public static GameObject[] waypointArray;
+	public static int enemiesCount = 0;
 
-        // Update player direction.
-        faceMouse();
-        // Fire button is pressed.
-        if (Input.GetButton("Fire1"))
-        {
-            // For now, we are only shooting a gun, later we may be utilizing other types
-            // of weapons. So this may get replaced with a call to a function that determines
-            // which action to take.
-            fireGun();
-        }
-    }
+	public Camera povCam;
+	public LineRenderer shootLine;
+	public Transform playerTransform;
 
-    public IEnumerator Bullet()
-    {
-        shootLine.enabled = true;
-        yield return gunTimeOut;
-        shootLine.enabled = false;
-    }
+	private WaitForSeconds gunTimeOut = new WaitForSeconds (0.5f);
 
-    private void faceMouse()
-    {
-        // Get the in-world mouse position using the screen mouse position.
-        Vector3 mousePosition = povCam.ScreenToWorldPoint(Input.mousePosition);
-        // The direction vector between the character and the mouse.
-        playerTransform.up = new Vector3(
-            mousePosition.x - playerTransform.position.x,
-            mousePosition.y - playerTransform.position.y,
-            0
-        );
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		shootLine = GetComponent<LineRenderer> ();
+		waypointArray = GameObject.FindGameObjectsWithTag ("Enemy");
+		Movement.enemiesCount = waypointArray.Length;
+	}
 
-    private void fireGun()
-    {
-        Vector3 screenPos = povCam.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(screenPos, playerTransform.up, 100);
-        Vector3 focus = povCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
-        StartCoroutine(Bullet());
-        shootLine.SetPosition(0, playerTransform.position);
-        if (hit)
-        {
-            shootLine.SetPosition(1, hit.point);
-            var enemy = hit.collider.GetComponent<Health>();
-            if (enemy != null)
-                enemy.Damage();
-        }
-        else
-        {
-            shootLine.SetPosition(1, focus + (playerTransform.up * 100));
-        }
-    }
+	// Update is called once per frame
+	void Update ()
+	{
+		//Update position of player
+		var currentPosition = playerTransform.position;
+		currentPosition.x += Input.GetAxisRaw ("Horizontal") * 5 * Time.deltaTime;
+		currentPosition.y += Input.GetAxisRaw ("Vertical") * 5 * Time.deltaTime;
+		playerTransform.position = new Vector3 (currentPosition.x, currentPosition.y);
 
-    
+		// Update player direction.
+		faceMouse ();
+
+		// Fire button is pressed.
+		if (Input.GetButton ("Fire1")) {
+			// For now, we are only shooting a gun, later we may be utilizing other types
+			// of weapons. So this may get replaced with a call to a function that determines
+			// which action to take.
+			fireGun ();
+		}
+	}
+
+	public IEnumerator Bullet ()
+	{
+		shootLine.enabled = true;
+		yield return gunTimeOut;
+		shootLine.enabled = false;
+	}
+
+	private void faceMouse ()
+	{
+		// Get the in-world mouse position using the screen mouse position.
+		Vector3 mousePosition = povCam.ScreenToWorldPoint (Input.mousePosition);
+		// The direction vector between the character and the mouse.
+		playerTransform.up = new Vector3 (
+			mousePosition.x - playerTransform.position.x,
+			mousePosition.y - playerTransform.position.y,
+			0
+		);
+	}
+
+	private void fireGun ()
+	{
+		Vector3 screenPos = povCam.ScreenToWorldPoint (Input.mousePosition);
+		RaycastHit2D hit = Physics2D.Raycast (screenPos, playerTransform.up, 100);
+		Vector3 focus = povCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0));
+		StartCoroutine (Bullet ());
+		shootLine.SetPosition (0, playerTransform.position);
+		if (hit) {
+			shootLine.SetPosition (1, hit.point);
+			var enemy = hit.collider.GetComponent<Health> ();
+			if (enemy != null)
+				enemy.Damage ();
+		} else {
+			shootLine.SetPosition (1, focus + (playerTransform.up * 100));
+		}
+	}
+
 }
