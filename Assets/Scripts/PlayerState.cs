be96cceb345d1445ce0ab20e3f3d6c53ed4a1042
DESKTOP-DHF1UVD\moham <mohammad.chowdhry@contax.com>
2017-10-16 20:58:06 -0400

    Fixed tabs issue. Changed enemy health to 200.

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class PlayerState : MonoBehaviour
 {
 
-    public int health = 200;
-    public int damageRate = 20;
+	public int health = 200;
+	public int damageRate = 20;
 
 
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
-    }
+	}
 
 }
