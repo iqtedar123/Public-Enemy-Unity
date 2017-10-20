f2089e3c9aed05e96a90f4779fe50ad3424881b2
DESKTOP-DHF1UVD\moham <mohammad.chowdhry@contax.com>
2017-10-20 00:48:36 -0400

    Created a start menu and modified navigation.

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -8,6 +8,11 @@ public class UIManager : MonoBehaviour {
     public static string gameOverReason;
     public Text gameOverReasonText;
     public Button retryButton;
+	public Button startGameButton;
+	public Button settingsButton;
+	public Button exitToMainButton;
+	public Text statusText;
+	public float statusTextDuration = 5; //Seconds to read the text
 	// Use this for initialization
 	void Start () {
         if (retryButton != null)
@@ -15,7 +20,22 @@ public class UIManager : MonoBehaviour {
             Button retryBtn = retryButton.GetComponent<Button>();
             retryBtn.onClick.AddListener(openLevel0);
         }
-    }
+		if(startGameButton != null)
+		{
+			Button startBtn = startGameButton.GetComponent<Button>();
+			startBtn.onClick.AddListener(openLevel);
+		}
+		if (settingsButton != null)
+		{
+			Button settingsBtn = settingsButton.GetComponent<Button>();
+			settingsBtn.onClick.AddListener(OpenSettings);
+		}
+		if(exitToMainButton != null)
+		{
+			Button exitButton = exitToMainButton.GetComponent<Button>();
+			exitButton.onClick.AddListener(openMainMenu);
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -34,4 +54,29 @@ public class UIManager : MonoBehaviour {
         gameOverReason = null;
         SceneManager.LoadScene("Level01");
     }
+	//Initial Level to be loaded will be set here. 
+	public void openLevel()
+	{
+		//Opens level when button is pressed in start menu. 
+		//TODO Change this later. 
+		SceneManager.LoadScene("Level01");
+	}
+	public void openMainMenu()
+	{
+		//Opens the main menu. 
+		SceneManager.LoadScene("Main_Menu");
+	}
+	//Opens the settings menu.
+	public void OpenSettings()
+	{
+		//TODO Create the settings menu. 
+		//TODO Open the settings menu. 
+
+		statusText.text = "Coming Soon!";
+		Invoke("HideStatusText", statusTextDuration);
+	}
+	public void HideStatusText()
+	{
+		statusText.text = "";
+	}
 }
