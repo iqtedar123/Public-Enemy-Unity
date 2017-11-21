91be3bc5fc1469ba289370af27dc54079799f587
Rajdeep Nanua <rajdeep.nanua@mail.utoronto.ca>
2017-11-20 23:46:08 -0500

    Made the cutscenes and shit

--- Assets/Scripts/Cutscene.cs
+++ Assets/Scripts/Cutscene.cs
@@ -1,29 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Cutscene : MonoBehaviour {
 
 	public Texture[] frames;
-	public float frameTime = 3.0f;
+	public float frameTime = 7.0f;
 	private int frame=0;
+	private float mouseDownTime = 1.0f;
+	private float mouseTime;
 	private float nextFrameTime;
+	public string levelname;
 
 	// Use this for initialization
 	void Start () {
-		nextFrameTime = Time.time + frameTime;
+		nextFrameTime = frameTime;
+		mouseTime = mouseDownTime;
 	}
 	
 	// Update is called once per frame
 	void OnGUI () {
 		if (frame < frames.Length) {
 			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), frames [frame]);
-			if (Time.time >= nextFrameTime) {
+			nextFrameTime -= Time.deltaTime;
+			mouseTime -= Time.deltaTime;
+			if (Input.GetMouseButtonDown (0) && mouseTime <= 0) {
 				frame++;
-				nextFrameTime += frameTime;
+				nextFrameTime = frameTime;
+				mouseTime = mouseDownTime;
+			} else if (nextFrameTime <= 0) {
+				frame++;
+				nextFrameTime = frameTime;
+				mouseTime = mouseDownTime;
+			} else if (Input.GetKeyDown (KeyCode.Escape)) {
+				frame = frames.Length + 1;
+				nextFrameTime = frameTime;
 			}
 		} else {
-			// Cutscene over
+			SceneManager.LoadScene (levelname);
 		}
 	}
 }
