27ed5805e620bd901c0011e1ecf3cde6ffd9a227
Darren Hobin <darrenhobin@live.com>
2017-11-20 20:31:25 -0500

    Demo cutscene
    If working on top of this, delete scene photos that this added.

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour {
+
+	public Texture[] frames;
+	public float frameTime = 3.0f;
+	private int frame=0;
+	private float nextFrameTime;
+
+	// Use this for initialization
+	void Start () {
+		nextFrameTime = Time.time + frameTime;
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		if (frame < frames.Length) {
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), frames [frame]);
+			if (Time.time >= nextFrameTime) {
+				frame++;
+				nextFrameTime += frameTime;
+			}
+		} else {
+			// Cutscene over
+		}
+	}
+}
